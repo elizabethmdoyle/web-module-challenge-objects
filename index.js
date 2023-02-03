5b718ef4819c3fd234caf5f6b02f3b24ae10e057
Elizabeth Doyle <myoddjourney@gmail.com>
2023-02-02 19:02:57 -0500

    5 tests passing

--- index.js
+++ index.js
@@ -54,9 +54,16 @@ const burger = {
   name: "Burger", 
   price: 18, 
   category: "Lunch", 
-  
+  discount: function(person) {
+    if(person === 'teacher' || person === 'student') {
+      return this.price - (this.price * 0.25);
+    } else if (person === 'public') {
+      return this.price - (this.price * 0.10);
+    } 
+  }
 }
 
+console.log(burger.discount('teacher'), 'task 2')
 
 
 
